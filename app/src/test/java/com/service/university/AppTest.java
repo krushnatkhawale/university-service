693f3e670baa22eda71e537005ed120c1414cc9c
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-08-06 22:38:59 +0100

    app: wrapper task removed

---
+++ app/src/test/java/com/service/university/AppTest.java
@@ -0,0 +1,15 @@
+package com.service.university;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.*;
+
+@SpringBootTest
+public class AppTest {
+
+    @Test
+    public void someTest(){
+
+    }
+}
