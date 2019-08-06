f6cb194f90d7f2915ead51400ee472a77e8d2f75
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-08-06 22:56:17 +0100

    app: springboottest: hello world test added

--- app/src/test/java/com/service/university/AppTest.java
+++ app/src/test/java/com/service/university/AppTest.java
@@ -1,15 +1,31 @@
 package com.service.university;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AppTest {
 
-    @Test
-    public void someTest(){
+    @LocalServerPort
+    int port;
 
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
+    @Test
+    public void someTest() {
+        ResponseEntity<String> forEntity = testRestTemplate.getForEntity("http://localhost:" + port + "/universities", String.class);
+        System.out.println("REPSONSE: " + forEntity.getStatusCodeValue());
+        assertEquals(HttpStatus.OK, forEntity.getStatusCode());
     }
 }
