5d4c0f9fb4617d328c72782dec3417203eb66c00
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-08-07 18:54:36 +0100

    app: test: util class added

--- app/src/test/java/com/service/university/model/Util.java
+++ app/src/test/java/com/service/university/model/Util.java
@@ -1,13 +1,39 @@
 package com.service.university.model;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class Util {
 
 
     public static void assertOk(HttpStatus statusCode) {
         assertEquals(HttpStatus.OK, statusCode);
+    }
+
+    public static JSONObject getRequestBodyAsJson(ResponseEntity<String> forEntity) {
+        try {
+            return new JSONObject(forEntity.getBody());
+        } catch (Exception e) {
+            fail("Error while parsing response to JSON: " + e.getMessage());
+        }
+        return null;
+    }
+
+    public static JSONArray getRequestBodyAsJsonArray(ResponseEntity<String> forEntity) {
+        try {
+            return new JSONArray(forEntity.getBody());
+        } catch (Exception e) {
+            fail("Error while parsing response to JSON: " + e.getMessage());
+        }
+        return null;
+    }
+
+    public static University getUniversity() {
+        return new University();
     }
 }
