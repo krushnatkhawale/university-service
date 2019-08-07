5a0470000f5a52f15cb3a947f70b4f6938964c96
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-08-07 20:53:28 +0100

    app: test: refactored

--- app/src/test/java/com/service/university/AppTest.java
+++ app/src/test/java/com/service/university/AppTest.java
@@ -1,7 +1,5 @@
 package com.service.university;
 
-import com.service.university.model.Response;
-import org.json.JSONArray;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,13 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static com.service.university.util.Util.*;
-import static java.util.Arrays.asList;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
