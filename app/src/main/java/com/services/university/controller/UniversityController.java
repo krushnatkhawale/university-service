236cf31e8ec81049f33519e3733b0a5222177f07
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-08-07 21:48:38 +0100

    app: get all endpoint added

--- app/src/main/java/com/services/university/controller/UniversityController.java
+++ app/src/main/java/com/services/university/controller/UniversityController.java
@@ -1,6 +1,7 @@
 package com.services.university.controller;
 
 import com.services.university.model.University;
+import com.services.university.service.UniversityService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,17 +18,23 @@ import static java.util.Arrays.asList;
 @RestController
 public class UniversityController {
 
+    private final UniversityService universityService;
+
+    public UniversityController(UniversityService universityService) {
+        this.universityService = universityService;
+    }
+
     @PostMapping("/universities")
     public ResponseEntity postUniversity(@RequestBody University university) {
         log.info("Post a university");
-        //  List<Student> allStudents = studentService.getAll();
+        universityService.save(university);
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @GetMapping("/universities")
     public ResponseEntity<List<University>> getUniversities() {
         log.info("Get all universities");
-        //  List<Student> allStudents = studentService.getAll();
-        return ResponseEntity.ok(asList());
+        List<University> universities = universityService.getAll();
+        return ResponseEntity.ok(universities);
     }
 }
